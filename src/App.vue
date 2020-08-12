1caeac602d8b45dae327d945cb732fbf99f16ab3
lp920133349 <920133349@qq.com>
2020-08-12 16:29:45 +0800

    罗鹏 16:29

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,31 @@
   </div>
 </template>
 
+<script>
+  // import Vue from 'vue';
+  // import VueResource from 'vue-resource'
+  // Vue.use(VueResource)
+  // 如果我们通过全局配置了，请求的数据接口 根域名，则 ，在每次单独发起 http 请求的时候，请求的 url 路径，应该以相对路径开头，前面不能带 /  ，否则 不会启用根路径做拼接；
+  // Vue.http.options.root = '';
+  // 全局启用 emulateJSON 选项
+  //Vue.http.options.emulateJSON = true;
+  export default {
+    name: '',
+    //存放 数据
+    data: function () {
+      return {
+      }
+    },
+    mounted() { 
+      alert()
+    },
+    //存放 方法
+    methods: {
+
+    },
+  }
+</script>
+
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
